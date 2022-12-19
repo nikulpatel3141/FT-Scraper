adeb404bc734c57450b4755d791244bd3b3c8034
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2022-12-19 19:44:50 +0000

    add readme; bugfix in workflow; formatting

---
+++ README.md
@@ -0,0 +1,15 @@
+# FT Market Data Scraper
+
+The `main.py` script queries an endpoint provided by [FT](https://www.ft.com/) for the daily returns of the instruments (hardcoded) in that script.
+
+## Automated Scraping
+
+The `scrape.yml` workflow file runs the script every 5 minutes and pushes the changes to the `master` branch.
+
+Adapted from this tutorial: https://www.swyx.io/github-scraping
+
+## Copyright
+
+As far as I can tell from the FT's copyright page this isn't not breaking any rules. If it is let me know and I'll take it down.
+
+Ref: https://help.ft.com/legal-privacy/copyright-policy/
--- main.py
+++ main.py
@@ -12,26 +12,38 @@ FT_SYMBOLS = {
     "GBPUSD": "GBPUSD",
     "Coffee": "KC.1:IUS",
     "Gold": "GC.1:CMX",
-} # FIXME: should be in a config file but more convenient here
-FT_ENDPOINT = "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes"
+}  # FIXME: should be in a config file but more convenient here
+FT_ENDPOINT = (
+    "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes"
+)
 
-def query_ft_mkt_data(symbols : list) -> dict:
+
+def query_ft_mkt_data(symbols: list) -> dict:
     """Query FT for market data quotes
-    
+
     Returns: a dict of quote data with the queried symbols as keys
     """
     endpoint = FT_ENDPOINT + "?symbols=" + ",".join(symbols)
     data = requests.get(endpoint).json()
-    data = data['data']['items']
-    return {sym: quote['quote'] for quote in data for sym in symbols if quote['symbolInput']==sym}
+    data = data["data"]["items"]
+    return {
+        sym: quote["quote"]
+        for quote in data
+        for sym in symbols
+        if quote["symbolInput"] == sym
+    }
 
 
-def main():    
+def main():
     ft_data = query_ft_mkt_data(list(FT_SYMBOLS.values()))
-    daily_ret = {disp_sym: ft_data[ft_sym]['change1DayPercent'] for disp_sym, ft_sym in FT_SYMBOLS.items()}
-    
+    daily_ret = {
+        disp_sym: ft_data[ft_sym]["change1DayPercent"]
+        for disp_sym, ft_sym in FT_SYMBOLS.items()
+    }
+
     daily_ret_json = json.dumps(daily_ret)
     print(daily_ret_json)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
